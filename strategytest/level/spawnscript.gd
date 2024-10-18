ce9e1dde77fd3677ca4937338e1a6f20c4a2d3ab
Tobias <118775721+HerrVonPoggins@users.noreply.github.com>
2024-10-18 17:34:02 +0200

    testspawner

--- strategytest/level/spawnscript.gd
+++ strategytest/level/spawnscript.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
-var crystals = 0
-var unit_counter = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,6 +8,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Label.set_text("kristalle: " + str(crystals)+ "   " + "FPS: " + str(Engine.get_frames_per_second()) + "   " + "Unitzahl: "+ str(unit_counter))
+	pass
 
 
+func _on_timer_timeout():
+	var unit = load("res://units/unit2.tscn").instantiate()
+	if $"..".unit_counter <= 300:
+		get_parent().add_child(unit)
+		$"..".unit_counter += 1
+	
