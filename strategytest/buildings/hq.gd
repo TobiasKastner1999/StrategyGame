d2f4eabb60588c1bad55ab448eee303043599ab1
Jakob Keber <jakob.keber@gmail.com>
2024-10-25 11:37:56 +0200

    Updated Worker Generation
    - workers are now spawned from the HQ automatically on a repeating timer
    - once the amount of workers has reached a certain number, no new workers will spawn

--- strategytest/buildings/hq.gd
+++ strategytest/buildings/hq.gd
@@ -1,8 +1,26 @@
 extends Node3D
 
-# spawn worker when needed
-func _on_button_pressed():
+const SPAWN_DELAY = 10.0
+const MAX_WORKERS = 4
+var current_workers = 0
+var can_spawn = false
+
+func _ready():
+	$SpawnTimer.start(SPAWN_DELAY)
+
+func _process(delta):
+	if can_spawn and current_workers < MAX_WORKERS:
+		spawnWorker()
+
+func spawnWorker():
+	can_spawn = false
+	$SpawnTimer.start(SPAWN_DELAY)
+	current_workers += 1
+	
 	var worker = load("res://units/worker.tscn").instantiate()
 	$Workers.add_child(worker)
 	worker.global_position = $SpawnPoint.global_position
 	worker.hq = self
+
+func _on_spawn_timer_timeout():
+	can_spawn = true
