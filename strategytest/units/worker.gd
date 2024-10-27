e946ebc9e3084c26d64b0b45aa8a4e6d783267b8
Jakob Keber <jakob.keber@gmail.com>
2024-10-27 12:00:31 +0100

    Updated Worker AI
    - workers will now only target the first closest resource, rather than constantly changing targets
    - also increased the workers' detection range

--- strategytest/units/worker.gd
+++ strategytest/units/worker.gd
@@ -12,6 +12,7 @@ var path = []
 var path_ind = 0
 var go_to = Vector3.ZERO
 var crystal = 0
+var target_resource
 var done = false
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -27,8 +28,37 @@ func _physics_process(delta):
 # gravity
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-# sets the movement of the unit and stops when close to goal
+	
+	if crystal != 0:
+		if global_position.distance_to(hq.global_position) < 4:
+			crystal = 0
+			Global.crystals += 1
+			done = false
+		else:
+			go_to = hq.global_position
+	
+	elif target_resource != null:
+		if global_position.distance_to(target_resource.global_position) <= 3 and done == false:
+			done = true
+			target_resource.get_parent().get_parent().resource -=1
+			crystal = 1
+			target_resource = null
+		else:
+			go_to = target_resource.global_position
+	
+	else:
+		var nearby_resources = $RangeArea.get_overlapping_bodies()
+		var closest_distance
+		var closest_resource
+		for res in nearby_resources:
+			if res.is_in_group("resource"):
+				if closest_distance == null or global_position.distance_to(res.global_position) < closest_distance:
+					closest_distance = global_position.distance_to(res.global_position)
+					closest_resource = res
+		if closest_resource != null:
+			target_resource = closest_resource
+	
+	# sets the movement of the unit and stops when close to goal
 	navi.target_position = go_to
 	var dir = navi.get_next_path_position() - global_position
 	dir = dir.normalized()
@@ -36,26 +66,6 @@ func _physics_process(delta):
 	velocity = velocity.lerp(dir * SPEED, 10 * delta)
 	if position.distance_to(go_to) < 2:
 		velocity = Vector3.ZERO
-
-# area checks all bodies inside coll and puts them into a array
-# array is checked for bodies in group resource and moves the unit automatically to the resource
-# resource is then collected and unit delivers the resource to the base
-	if crystal != 0:
-		go_to = hq.global_position
-		if global_position.distance_to(hq.global_position) < 4:
-			crystal = 0
-			Global.crystals += 1
-			done = false
-	
-	else:
-		var list = $RangeArea.get_overlapping_bodies()
-		for n in list:
-			if (n.is_in_group("resource")):
-				go_to = n.global_position
-				if global_position.distance_to(n.global_position) <= 3 and done == false:
-					done = true
-					n.get_parent().get_parent().resource -=1
-					crystal = 1
 
 	move_and_slide()
 
