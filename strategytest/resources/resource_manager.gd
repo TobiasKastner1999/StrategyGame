0d7526ab2c356ce7f0da611f2e3f663e56268634
Jakob Keber <jakob.keber@gmail.com>
2024-10-30 09:42:41 +0100

    Updated Navmesh
    converted the main node into the scene's navmesh

--- strategytest/resources/resource_manager.gd
+++ strategytest/resources/resource_manager.gd
@@ -2,5 +2,5 @@ extends Node
 
 # calls for the HQ to have its workers remove references to a removed resource node
 func _on_ressource_expended(node):
-	$".."/Nav/HQFriendly.excludeResource(node)
-	$".."/Nav/HQEnemy.excludeResource(node)
+	$".."/HQFriendly.excludeResource(node)
+	$".."/HQEnemy.excludeResource(node)
