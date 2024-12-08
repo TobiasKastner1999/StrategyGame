d42fe6192ec175c222dfe2af00a59bde4e231250
Jakob Keber <jakob.keber@gmail.com>
2024-12-08 14:31:36 +0100

    Adjustments
    - removed obsolete code
    - re-ordered functions in the worker script
    - added code comments
    - converted worker behaviours into new scenes

--- strategytest/Scenes & Scripts/Behaviours/resource_target_behaviour.gd
+++ strategytest/Scenes & Scripts/Behaviours/resource_target_behaviour.gd
@@ -6,60 +6,70 @@ var run_node : Node3D # the node the behaviour is operating on
 func runBehaviour(node):
 	run_node = node # stores the operating node
 	
-	if hasEmptyTarget() and notInAcquisition():
-		match checkCarryState():
+	if hasEmptyTarget() and notInAcquisition(): # runs the behaviour if the node has no target, and is not currently acquiring resources
+		match checkCarryState(): # checks the node's carry state
 			0:
-				targetCrystal()
+				targetResource() # targets a resource if the node is not carrying any
 			1:
-				targetHQ()
+				targetHQ() # target's the node's base if it is carrying resources
 
+# checks if the node has no target
 func hasEmptyTarget():
 	if run_node.getDestination() == null or run_node.getDestination() == run_node.getPosition():
-		return true
+		return true # returns true of the node has no active destination, or its destination is its own position
 	return false
 
+# checks if the node is not currently acquiring a resource
 func notInAcquisition():
 	if run_node.getInteractionState() != 1:
-		return true
+		return true # returns true of the node is not in the acquisition state of its interaction
 	return false
 
+# checks the node's resource carry state
 func checkCarryState():
 	return run_node.getResourceState()
 
-func targetCrystal():
+# attempts to have the node target a resource
+func targetResource():
 	if checkForPrevious():
-		reTargetPrevious()
+		reTargetPrevious() # if the node has a previous saved resource node, targets that node
 	elif checkForKnown():
-		targetKnown()
+		targetKnown() # otherwise, targets a different resource node that the node has saved
 
+# checks if the node has a previously saved resource node target
 func checkForPrevious():
 	if is_instance_valid(run_node.getPrevious()):
-		return true
+		return true # returns true if a saved target exists and is still valid
 	return false
 
+# has the node target its previous target
 func reTargetPrevious():
 	run_node.setTarget(run_node.getPrevious())
 
+# checks if the node knows of any other resource nodes
 func checkForKnown():
 	if run_node.getKnownResources().size() > 0:
-		return true
+		return true # returns true if there are resources saved on the node
 	return false
 
+# has the node target a saved resource from its internal list
 func targetKnown():
-	var known = run_node.getKnownResources()
+	var known = run_node.getKnownResources() # retrieves the list of known resources
 	var target
 	var distance
 	
+	# then checks for each resource node in the list
 	for resource in known:
-		if is_instance_valid(resource):
+		if is_instance_valid(resource): # if the node is still valid, proceeds with the check
 			var resource_distance = run_node.getPosition().distance_to(resource.global_position)
 			if target == null or resource_distance < distance:
-				target = resource
+				target = resource # overwrites the intended target if no intended target exists yet, or if the new target is closer than the previous
 				distance = resource_distance
 		else:
-			run_node.removeResourceKnowledge(resource)
+			run_node.removeResourceKnowledge(resource) # otherwise clears the resource from the node's list
 	
-	run_node.setTarget(target)
+	run_node.setTarget(target) # has the node target the intended target
 
+# has the node target its base
 func targetHQ():
 	run_node.setTarget(run_node.getHQ())
--- strategytest/Scenes & Scripts/Prefabs/Units/Worker/worker.gd
+++ strategytest/Scenes & Scripts/Prefabs/Units/Worker/worker.gd
@@ -5,9 +5,9 @@ signal deleted(worker) # to tell the system that the worker has been removed
 const TARGET_TYPE = "worker" # the worker's combat type
 const SPEED = 10.0 # the worker's movement speed
 const MAX_HP = 2.0 # the worker's maximum hit points
-const MINE_SPEED = 5.0
-const INTERACTION_STATE_MAX = 2
-const PROXIMITY_DISTANCE = 5.0
+const MINE_SPEED = 5.0 # the speed at which the worker acquired resources
+const INTERACTION_STATE_MAX = 2 # the maximum value of the worker's interaction state
+const PROXIMITY_DISTANCE = 5.0 # the worker's proximity distance
 
 var SR
 var faction = 0 # which faction does the worker belong to?
@@ -17,10 +17,9 @@ var path_ind = 0 # the index of the worker's current path node
 var resource = [0, 0] # what type of resource is the worker carrying, and how much of it?
 var known_resources = [] # an array of all resource nodes the worker has discovered
 var priority_movement = false # is the worker's currently overwritten by a player input?
-var has_moved = false
 var destination # the worker's current navigation destination
-var target_node
-var previous_target
+var target_node # the node the worker currently is targeting
+var previous_target # the worker's previously targeted resource
 
 @onready var hp = MAX_HP # the worker's current hit points, set to the maximum on instantiation
 @onready var navi : NavigationAgent3D = $NavAgent # the navigation agent controlling the worker's movement
@@ -33,87 +32,86 @@ func _ready():
 	await get_tree().physics_frame
 	velocity = Vector3.ZERO
 
-# controls the worker's movement and other actions
+# controls the worker's behaviour
 func _physics_process(delta):
-	await $UnitBehaviours.runBehaviours(self, delta)
-	animationControl()
+	await $UnitBehaviours.runBehaviours(self, delta) # calls the worker's behaviour tree
+	animationControl() # then plays the correct animation based on the worker's current state
 
+# controls the worker's animation
+func animationControl():
+	if interaction_state == 1:
+		$rebel_anim/AnimationPlayer.play("attack") # plays the attack animation if the worker is mining
+	elif velocity != Vector3.ZERO:
+		$rebel_anim/AnimationPlayer.play("walk") # plays the walk animation if they are moving
+	else:
+		$rebel_anim/AnimationPlayer.play("idle") # plays the idle animation otherwise
+
+# calls the worker to start mining
 func startMiningState():
 	$MiningTimer.start(MINE_SPEED)
 	await get_tree().create_timer(1).timeout
 	Sound.play_sound("res://Sounds/MiningSound_Rebells_FreeSoundCommunity.mp3")
 
-func getPosition():
-	return global_position
+# advanced the worker's interaction state
+func advanceInteractionState():
+	if interaction_state < INTERACTION_STATE_MAX:
+		interaction_state += 1 # increments the state if it is not at its max value
+	else:
+		interaction_state = 0 # resets it otherwise
 
-func getDestination():
-	return destination
-
-func getResourceState():
-	if resource == [0, 0]:
-		return 0
-	return 1
-
-func getNavigationAgent():
-	return navi
-
-func getMovementSpeed():
-	return SPEED
-
-func getProximity():
-	return PROXIMITY_DISTANCE
-
-func getActiveTarget():
-	return target_node
-
+# returns the worker's current interaction state
 func getInteractionState():
 	return interaction_state
 
-func getPrevious():
-	return previous_target
-
-func getKnownResources():
-	return known_resources
-
-func getHQ():
-	return hq
-
-func getResource():
-	return resource
-
-func setResource(new_resource):
-	resource = new_resource
-
-func advanceInteractionState():
-	if interaction_state < INTERACTION_STATE_MAX:
-		interaction_state += 1
+# checks if the worker is currently doing anything
+func isWorking():
+	if known_resources.size() > 0 or resource != [0, 0] or priority_movement:
+		return true # returns true if the worker is delivering a crystal or knows of any remaining resource nodes
 	else:
-		interaction_state = 0
+		return false
 
+# sets the position the NavAgent will move to
+func setTargetPosition(target):
+	target_node = null
+	priority_movement = true
+	destination = target
+
+# sets the worker's target node
 func setTarget(target):
 	if target_node != null and str(target_node.getType()) == "resource":
-		previous_target = target_node
+		previous_target = target_node # stores the previous target if it was a resource
 	target_node = target
-	setDestination(target_node.global_position)
+	setDestination(target_node.global_position) # also sets target's position as new movement destination
 
+# sets the worker's movement destination
 func setDestination(new_destination):
 	destination = new_destination
-	if !has_moved:
-		has_moved = true
 
-func animationControl():
-	if interaction_state == 1:
-		$rebel_anim/AnimationPlayer.play("attack")
-	elif velocity != Vector3.ZERO:
-		$rebel_anim/AnimationPlayer.play("walk")
-	else:
-		$rebel_anim/AnimationPlayer.play("idle")
+# returns the worker's current movement destination
+func getDestination():
+	return destination
 
 # receives the path from NavAgent
 func move_to(target_pos):
 	path = navi.get_simple_path(global_transform.origin, target_pos)
 	path_ind = 0
-	
+
+# returns the worker's current position
+func getPosition():
+	return global_position
+
+# returns the worker's navigation agent
+func getNavigationAgent():
+	return navi
+
+# returns the worker's current movement speed
+func getMovementSpeed():
+	return SPEED
+
+# returns the worker's current proximity distance
+func getProximity():
+	return PROXIMITY_DISTANCE
+
 # causes the worker to take a given amount of damage
 func takeDamage(damage, _attacker):
 	hp -= damage # subtracts the damage taken from the current hp
@@ -126,6 +124,41 @@ func takeDamage(damage, _attacker):
 # sets attack target (has no effect for the worker)
 func setAttackTarget(_unit):
 	pass
+
+# updates the worker's carried resource
+func setResource(new_resource):
+	resource = new_resource
+
+# returns the worker's currently carried resource
+func getResource():
+	return resource
+
+# returns the state of the worker's currently carried resource
+func getResourceState():
+	if resource == [0, 0]:
+		return 0
+	return 1
+
+# returns the worker's current target node
+func getActiveTarget():
+	return target_node
+
+# returns the worker's saved previous resource node target
+func getPrevious():
+	return previous_target
+
+# returns a list of the resources the worker knows
+func getKnownResources():
+	return known_resources
+
+# removes a cleared resource node from the worker's list if it is on there
+func removeResourceKnowledge(removed_resource):
+	if known_resources.has(removed_resource):
+		known_resources.erase(removed_resource)
+
+# returns the worker's HQ
+func getHQ():
+	return hq
 
 # sets the worker's faction to a given value
 func setFaction(f : int):
@@ -147,13 +180,6 @@ func getType():
 func getSize():
 	return $WorkerColl.shape.radius
 
-# checks if the worker is currently doing anything
-func isWorking():
-	if known_resources.size() > 0 or resource != [0, 0] or priority_movement:
-		return true # returns true if the worker is delivering a crystal or knows of any remaining resource nodes
-	else:
-		return false
-
 # changes the color of the worker when selected
 func select():
 	pass
@@ -164,21 +190,11 @@ func deselect():
 	pass
 	$rebel_anim/Armature_002/Skeleton3D/WorkerBody.material_override = load(Global.getFactionColor(faction))
 
-# sets the position the NavAgent will move to
-func setTargetPosition(target):
-	target_node = null
-	priority_movement = true
-	destination = target
-
-# removes a cleared resource node from the worker's list if it is on there
-func removeResourceKnowledge(removed_resource):
-	if known_resources.has(removed_resource):
-		known_resources.erase(removed_resource)
-
 # if a new resource entered the worker's detection radius, adds it to its list
 func _on_range_area_body_entered(body):
 	if body.is_in_group("resource") and !known_resources.has(body):
 		known_resources.append(body)
 
+# advances the worker's interaction state once it has finished mining
 func _on_mining_timer_timeout():
 	advanceInteractionState()
