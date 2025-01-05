b265c6ce6e8794dfbb1e41a80512bc8b601e22f3
Jakob Keber <jakob.keber@gmail.com>
2025-01-05 13:53:52 +0100

    Added Multi-Support Framework to Selected Panel
    added the framework to have the selected panel support different types of game objects

--- strategytest/Scenes & Scripts/Main Scene/UI/selected_panel.gd
+++ strategytest/Scenes & Scripts/Main Scene/UI/selected_panel.gd
@@ -13,18 +13,8 @@ func activatePanel(selected):
 	if current_selected != null:
 		unselect() # unselects the currently selected object (if one exists)
 	current_selected = selected # saves the selected object
+	
 	updateSelectedInterface() # updates the changing interface properties
-	if selected.has_method("getProduction"):
-		for type in Global.unit_dict:
-			if type != "worker": 
-				var button = load("res://Scenes & Scripts/Prefabs/Interface/unit_type_button.tscn").instantiate()
-				$ButtonContainer.add_child(button) # sets up a button for each unit type in the data
-				button.assignType(type) # assigns the button to the correct unit type
-				button.change_type.connect(_on_type_button_pressed)
-				button.hover_tooltip.connect(_on_type_button_hover)
-		for button in $ButtonContainer.get_children():
-			if button.getType() == current_selected.getProduction():
-				button.grab_focus()
 	self.visible = true
 	selected.interface_update.connect(updateSelectedInterface) # connects object for dynymic updates
 
@@ -40,15 +30,31 @@ func unselect():
 func updateSelectedInterface():
 	if current_selected == null:
 		return
-	if current_selected.hp <= 0:
+	elif current_selected.hp <= 0:
 		unselect()
 	else:
 		$SelectedName.text = "[b]" + Global.getText(current_selected.DISPLAY_NAME) + "[/b]" # displays the name
 		$SelectedHP.text = Global.getText("@inspect_text_hp") + ": " + str(current_selected.hp) + "/" + str(current_selected.MAX_HP) # displays the object's current hp out of its maximum hp
-		if current_selected.getStatus():
-			$SelectedStatus.text = Global.getText("@inspect_text_status") + ": " + Global.getText("@inspect_text_status_true") # displays the object's active status
-		else:
-			$SelectedStatus.text = Global.getText("@inspect_text_status") + ": " + Global.getText("@inspect_text_status_false") # displays the object's inactive status
+		
+		match current_selected.getType():
+			"building":
+				if current_selected.getStatus():
+					$SelectedStatus.text = Global.getText("@inspect_text_status") + ": " + Global.getText("@inspect_text_status_true") # displays the object's active status
+				else:
+					$SelectedStatus.text = Global.getText("@inspect_text_status") + ": " + Global.getText("@inspect_text_status_false") # displays the object's inactive status
+				
+				for type in Global.unit_dict:
+					if type != "worker": 
+						var button = load("res://Scenes & Scripts/Prefabs/Interface/unit_type_button.tscn").instantiate()
+						$ButtonContainer.add_child(button) # sets up a button for each unit type in the data
+						button.assignType(type) # assigns the button to the correct unit type
+						button.change_type.connect(_on_type_button_pressed)
+						button.hover_tooltip.connect(_on_type_button_hover)
+				for button in $ButtonContainer.get_children():
+					if button.getType() == current_selected.getProduction():
+						button.grab_focus()
+			"hq":
+				pass
 
 # calls to toggle the selected object's status when the button is pressed
 func _on_button_toggle_pressed():
