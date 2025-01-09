a5127d63aba5a06834beefb88ab887776265ef64
Jakob Keber <jakob.keber@gmail.com>
2025-01-09 10:56:08 +0100

    Added Info Text to Unit Production

--- strategytest/Scenes & Scripts/Main Scene/UI/tooltip_panel.gd
+++ strategytest/Scenes & Scripts/Main Scene/UI/tooltip_panel.gd
@@ -7,10 +7,10 @@ func setType(type):
 	current_type = type
 	
 	$TooltipHP.text = "[b]HP:[/b] " + str(Global.unit_dict[str(current_type)]["max_hp"])
-	$TooltipDamage.text = "[b]DP:[/b] " + str(Global.unit_dict[str(current_type)]["damage_value"])
-	$TooltipRange.text = "[b]RA:[/b] " + str(Global.unit_dict[str(current_type)]["attack_range"])
-	$TooltipAttackRate.text = "[b]AR:[/b] " + str(Global.unit_dict[str(current_type)]["attack_speed"])
-	$TooltipVision.text = "[b]VI:[/b] " + str(Global.unit_dict[str(current_type)]["detection_range"])
-	$TooltipSpeed.text = "[b]SP:[/b] " + str(Global.unit_dict[str(current_type)]["speed"])
-	$TooltipCost.text = "[b]CO:[/b] " + str(Global.unit_dict[str(current_type)]["resource_cost"])
-	$TooltipProduction.text = "[b]PR:[/b] " + str(Global.unit_dict[str(current_type)]["production_speed"])
+	$TooltipDamage.text = "[b]Dmg:[/b] " + str(Global.unit_dict[str(current_type)]["damage_value"])
+	$TooltipRange.text = "[b]Range:[/b] " + str(Global.unit_dict[str(current_type)]["attack_range"])
+	$TooltipAttackRate.text = "[b]AtSp:[/b] " + str(Global.unit_dict[str(current_type)]["attack_speed"])
+	$TooltipVision.text = "[b]Vis:[/b] " + str(Global.unit_dict[str(current_type)]["detection_range"])
+	$TooltipSpeed.text = "[b]Sp:[/b] " + str(Global.unit_dict[str(current_type)]["speed"])
+	$TooltipCost.text = "[b]Cost:[/b] " + str(Global.unit_dict[str(current_type)]["resource_cost"])
+	$TooltipProduction.text = "[b]PrTi:[/b] " + str(Global.unit_dict[str(current_type)]["production_speed"])
