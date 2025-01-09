a146ca3112ffc7fa86196e5dab491e605a9946b8
Tobias <118775721+HerrVonPoggins@users.noreply.github.com>
2025-01-09 21:32:07 +0100

    fog of war enabled
    -fog of war is on but not synched whith new positions

--- strategytest/Scenes & Scripts/Main Scene/main_scene.gd
+++ strategytest/Scenes & Scripts/Main Scene/main_scene.gd
@@ -109,9 +109,8 @@ func _on_language_changed():
 
 # updates the map with a new fog of war texture
 func _on_fog_of_war_fow_updated(new_texture):
-	pass
-	#$Map/Map/Bake.get_material_override().next_pass.set_shader_parameter("mask_texture", new_texture)
-	#$MiniMap.setFogTexture(new_texture)
+	$Map/Map/Bake.get_material_override().next_pass.set_shader_parameter("mask_texture", new_texture)
+	$MiniMap.setFogTexture(new_texture)
 
 # clears the inspect interface when the camera calls a new selection
 func _on_camera_clear_interface():
