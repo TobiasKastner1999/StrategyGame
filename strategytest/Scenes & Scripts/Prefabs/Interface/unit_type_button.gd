fcbf68fa49575be7c3c821e653ce455d778790b6
Tobias <118775721+HerrVonPoggins@users.noreply.github.com>
2025-01-13 12:29:51 +0100

    ui

--- strategytest/Scenes & Scripts/Prefabs/Interface/unit_type_button.gd
+++ strategytest/Scenes & Scripts/Prefabs/Interface/unit_type_button.gd
@@ -1,14 +1,27 @@
-extends Button
+extends TextureButton
 
 signal change_type(type) # to tell the interface that a production type change has been requested
 signal hover_tooltip(type, bol) # to tell the interface that the player is hovering over a production type
 
 var unit_type : int # the unit type this button represents
 
+func _ready():
+	Global.unit_dict["0"]["texture_normal"] = load("res://Assets/UI/OL_Fighter_UI.png")
+	Global.unit_dict["0"]["texture_pressed"] = load("res://Assets/UI/OL_Fighter_UI_pressed.png")
+	Global.unit_dict["1"]["texture_normal"] = load("res://Assets/UI/OL_GunVehicle_UI.png")
+	Global.unit_dict["1"]["texture_pressed"] = load("res://Assets/UI/OL_Fighter_UI_pressed.png")
+	Global.unit_dict["2"]["texture_normal"] = load("res://Assets/UI/OL_Fighter_UI.png")
+	Global.unit_dict["2"]["texture_pressed"] = load("res://Assets/UI/OL_Fighter_UI_pressed.png")
+	Global.unit_dict["3"]["texture_normal"] = load("res://Assets/UI/OL_Fighter_UI.png")
+	Global.unit_dict["3"]["texture_pressed"] = load("res://Assets/UI/OL_Fighter_UI_pressed.png")
+
+
 # sets the button's unit type
 func assignType(type):
 	unit_type = int(type)
-	text = Global.getText(Global.unit_dict[type]["name"]) # also displays the correct name of the unit
+	#text = Global.getText(Global.unit_dict[type]["name"]) # also displays the correct name of the unit
+	texture_normal = Global.unit_dict[type]["texture_normal"]
+	texture_pressed = Global.unit_dict[type]["texture_pressed"]
 
 # returns the unit type represented by this button
 func getType():
