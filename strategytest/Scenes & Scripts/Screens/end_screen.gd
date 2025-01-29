6593d7954d1a06841db83e5fd46eeeacf8f6b0be
Tobias <118775721+HerrVonPoggins@users.noreply.github.com>
2025-01-29 14:48:37 +0100

    update
    -new button for endscreen

--- strategytest/Scenes & Scripts/Screens/end_screen.gd
+++ strategytest/Scenes & Scripts/Screens/end_screen.gd
@@ -6,6 +6,7 @@ func _ready():
 
 func setTexts(): # sets the text for the quit button
 	$Button.text = Global.getText("@interface_button_quit_game")
+	$Menu.text = Global.getText("@interface_button_main_menu")
 
 
 func setText():
@@ -28,3 +29,11 @@ func _on_button_pressed(): # ends the game
 	Sound.play_sound_all("res://Sounds/Button Sound Variante 1.mp3",$"." ) # plays the button sound
 	await get_tree().create_timer(0.5).timeout # stalls the scene for button sound
 	get_tree().quit() # ends the game
+
+
+func _on_menu_pressed():
+	Sound.play_sound_all("res://Sounds/Button Sound Variante 1.mp3",$"." )
+	await get_tree().create_timer(0.5).timeout
+	get_tree().paused = false # upause the game so that the startscreen isnt frozen
+	Balance.reset()
+	get_tree().change_scene_to_file("res://Scenes & Scripts/Screens/start_screen.tscn") # changes scene to startscreen
